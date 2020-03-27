db02bf61b32c689d2c466f2048ff0d4b210da91c
pphelps28 <p.w.phelps28@gmail.com>
2020-03-26 20:38:20 -0400

    Added Footer

--- client/src/App.js
+++ client/src/App.js
@@ -8,17 +8,19 @@ import Work from './components/Work'
 import Play from './components/Play';
 import Contact from './components/Contact'
 import Background from './components/Background'
+import Footer from './components/Footer'
 import homeImage from './images/home.svg'
-import aboutImage from './images/about.jpg'
-import workImage from './images/work.jpg'
+import aboutImage from './images/about.png'
+import workImage from './images/work.png'
 import playImage from './images/play.svg'
 
 class App extends Component {
   render() {
     return (
       <>
-        <Contact toggleContact={this.props.toggleContact} visibility={this.props.visibility} sendContact={this.props.sendContact}/>
+        <Contact toggleContact={this.props.toggleContact} visibility={this.props.visibility} sendContact={this.props.sendContact} />
         <Navbar currentPage={this.props.currentPage} highlightHeader={this.props.highlightHeader} setPage={this.props.setPage} toggleContact={this.props.toggleContact} />
+        <Footer />
         <div id="content-area" >
           < Route exact path='/'>
             <Background animation={this.props.panin} image={homeImage} />
@@ -26,7 +28,7 @@ class App extends Component {
           </Route >
           <Route path='/about'>
             <Background animation={this.props.fadein} image={aboutImage} />
-            <About animation={this.props.slideup} image={aboutImage}/>
+            <About animation={this.props.slideup} image={aboutImage} />
           </Route>
           <Route path='/work'>
             <Background animation={this.props.rotatein} image={workImage} />
