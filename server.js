8ef29b01dd88a1adbd8741dd4f3d86b58ed8ecd7
pphelps28 <p.w.phelps28@gmail.com>
2020-05-08 12:53:36 -0400

    HTTPS sslify security

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const port = process.env.PORT || 5000
+const http = require('http')
+const enforce = require('express-sslify')
 app.use(express.static(path.resolve('./client/build')))
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve('./client/build/index.html'))
 })
-app.listen(port, () => {
+
+http.createServer(app).listen(app.get('port'), function () {
     console.log("Listening on port: " + port)
 })
